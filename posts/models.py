b73ef35c38f083fc8df7af291e0771eab85f453f
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-08-08 12:56:54 +0600

    project model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from categories.models import Category
+from author.models import Author
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharFiels(max_length=200)
+    content = models.TextField()
+    author = models.TextFiels()
+    category = models.ManyToManyField(Category) #ekta post multiple category er modhe thakte pare ekta category er modhe multiple oost thakte pare
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    
+    
+
+

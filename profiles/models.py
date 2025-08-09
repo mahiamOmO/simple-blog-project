9b56565b56028956eb4514c71c46a0b1d5163ce4
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-08-09 19:24:28 +0600

    profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from author.models import Author
+from author.models import Profile 
 
 # Create your models here.
 class Profile(models.Model):
     name = models.CharField(max_length=100)
     about = models.TextField()
-    author = models.OneToOneField(Author,on_delete=models.CASCADE,default=None)
+    author = models.OneToOneField(Profile,on_delete=models.CASCADE,default=None)
 
 
     def __str__(self):
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, redirect
+from . import forms
 
-# Create your views here.
-
-def add_profile(request):
-    return HttpResponse("This is the Add Profile page")
-
+def add_author(request):
+    if request.method == 'POST':
+        profile_form = forms.ProfileForm(request.POST)
+        if profile_form.is_valid():
+            profile_form.save()
+            return redirect('add_profile')
+    else:
+        profile_form = forms.ProfileForm()
+    return render(request, 'add_profile_html', {'form': profile_form})

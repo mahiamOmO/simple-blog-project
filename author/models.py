2ea1d340c0ac9b2dd3af8c47a29d823df8785af2
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-08-09 16:39:20 +0600

    models.py

--- author/models.py
+++ author/models.py
@@ -5,7 +5,7 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField()
     phone_no = models.CharField(max_length=12)
-    email = models.EmailField(max_length=254) 
+    email = models.EmailField(max_length=255,blank=True) 
 
     def __str__(self):
         return self.name
